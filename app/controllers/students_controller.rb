2d6316199e6299d913c95118dbc7278ee30b6477
Olivia Radick <olivia.radick@gmail.com>
2023-01-31 11:24:16 -0500

    commit tree 3f4d440552bb0caa443705237cad488855af8f0e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
-  def index
+# index is used to show everything
+  def index 
     students = Student.all
     render json: students
   end
-
+  
+  # show is used to show a specific thing
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
